6dc082057536c06cf5c3146b3c95ac9e2fece199
OmoniyIfeAbraham <ife04abraham@gmail.com>
2025-02-21 19:06:03 +0100

    Working on SignIn Page

--- src/components/Screens/Components/AuthComp/AuthNavComp.jsx
+++ src/components/Screens/Components/AuthComp/AuthNavComp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { ArrowLeft } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 import { Colors } from "../../../Utils/Colors";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -10,23 +11,54 @@ const Header = () => {
         <img
           src={require("./../../../../Assets/Images/Auth/Group 2076.png")}
           alt="Logo"
-          className="h-screen w-full"
+          className="h-screen"
         />
 
         {/* Navbar */}
-        <div className="absolute top-4 left-4 flex items-center gap-4">
+        <div className="absolute top-4 left-4 flex flex-row items-center justify-center gap-4">
           {/* Back Icon */}
-          <div className="border-2 border-gray-500 rounded-full p-2 cursor-pointer hover:bg-gray-200">
-            <ArrowLeft size={24} />
-          </div>
+          <Link
+            className="border-2 rounded-full p-2 cursor-pointer"
+            style={{
+              backgroundColor: Colors.White,
+              borderColor: Colors.Primary,
+            }}
+            to="/"
+          >
+            <ChevronLeft size={24} color={Colors.Primary} />
+          </Link>
 
           {/* Logo */}
           <img
-            src={require("./../../../../Assets/Images/Logo.png")}
+            src={require("./../../../../Assets/Images/Auth/Lexigo Logo 1.png")}
             alt="Logo"
             className="h-10"
           />
         </div>
+      </div>
+      {/* Right Section */}
+      <div>
+        {/* title */}
+        <div className='flex flex-col items-center'>
+          <h1
+            className="font-[Nunito] text-3xl font-bold"
+            style={{ color: Colors.Black }}
+          >
+            Hello Friend!
+          </h1>
+          <h3
+            className="font-[Nunito] text-xl font-bold"
+            style={{ color: Colors.Black }}
+          >
+            Login to Learn and Play.
+          </h3>
+        </div>
+        {/* form */}
+        <form>
+          <div>
+            
+          </div>
+        </form>
       </div>
     </div>
   );
