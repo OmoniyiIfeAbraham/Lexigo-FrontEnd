1ea10df8fd18c8f93c8c4418edc41ee9eab45bb4
OmoniyIfeAbraham <ife04abraham@gmail.com>
2025-03-02 13:50:11 +0100

    working on home page

--- src/components/Screens/Home/HomePage.jsx
+++ src/components/Screens/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Sidebar from "../Components/HomeComps/SidebarComp";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { Colors } from "../../Utils/Colors";
 
 const Home = () => {
   const [takenQuiz, setTakenQuiz] = useState(false);
   const [type, setType] = useState("");
+  const [showPopup, setShowPopup] = useState(true);
   const location = useLocation();
 
   let pass = location.state?.pass;
@@ -16,9 +18,54 @@ const Home = () => {
         {" "}
         {/* Content Area */}
         <div>
-          <h1>Home Page</h1>
+          <h1
+            className="font-[Nunito] font-bold"
+            style={{ fontSize: 24, color: Colors.Black }}
+          >
+            Learning Paths
+          </h1>
+          <p
+            className="font-[Nunito]"
+            style={{ fontSize: 16, fontWeight: "normal", color: Colors.Black }}
+          >
+            Select your type then begin to learn and play
+          </p>
         </div>
       </main>
+
+      {/* popup */}
+      {showPopup && (
+        <div
+          onClick={() => setShowPopup(false)}
+          className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+        >
+          {/* Popup Container */}
+          <div className="popup bg-white p-8 shadow-lg relative w-104 text-start">
+            <p
+              className="text-lg font-semibold font-[Nunito]"
+              style={{ color: Colors.Black }}
+            >
+              Hi there, Welcome to Lexigo.
+              <br />
+              <Link
+                to="/quiz"
+                className="font-[Nunito]"
+                style={{ color: Colors.Primary }}
+              >
+                Take the quiz
+              </Link>{" "}
+              if you have not 😁
+            </p>
+
+            {/* Dog Image (positioned outside but touching bottom-left border of popup) */}
+            <img
+              src={require("./../../../Assets/Images/HomePage/Lexigo dog 2.png")}
+              alt="Dog"
+              className="absolute -bottom-12 -left-20 w-20 h-20"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
