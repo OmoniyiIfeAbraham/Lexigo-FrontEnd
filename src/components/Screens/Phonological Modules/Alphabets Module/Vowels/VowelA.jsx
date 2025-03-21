c2d226dbc35f2112b091740ac8374f2f0679a78c
OmoniyIfeAbraham <ife04abraham@gmail.com>
2025-03-21 02:27:14 +0100

    Vowela into page

--- src/components/Screens/Phonological Modules/Alphabets Module/Vowels/VowelA.jsx
+++ src/components/Screens/Phonological Modules/Alphabets Module/Vowels/VowelA.jsx
@@ -1,8 +1,12 @@
-import { Menu, X } from "lucide-react";
-import React from "react";
+import { ChevronLeft, ChevronRight, Menu, X } from "lucide-react";
+import React, { useState } from "react";
 import { Colors } from "../../../../Utils/Colors";
+import { useNavigate } from "react-router-dom";
 
 const VowelA = () => {
+  const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(true);
+
   return (
     <div
       style={{
@@ -13,25 +17,86 @@ const VowelA = () => {
       }}
       className="w-full min-h-screen relative"
     >
-      <div className="content relative z-10 h-full p-14">
+      <div className="content relative z-10 h-full">
         {/* header */}
-        <div className="flex w-full h-[75px] items-center justify-between">
+        <div className="flex w-full h-[75px] items-center justify-between px-[110px] pt-24">
           <button
             className="navBtn w-[75px] h-[75px] flex justify-center items-center"
             style={{ backgroundColor: Colors.BeastyBrown2 }}
-            onClick={() => {}}
+            onClick={() => {
+              navigate("/phonological-path/alphabet");
+            }}
           >
             <X size={46} color={Colors.White} className="icon" />
           </button>
           <button
             className="navBtn w-[75px] h-[75px] flex justify-center items-center"
-            style={{ backgroundColor: Colors.BeastyBrown2 }}
+            style={{ backgroundColor: Colors.Pompelmo }}
             onClick={() => {}}
           >
             <Menu size={46} color={Colors.White} className="icon" />
           </button>
         </div>
+        {/* body */}
+        <div className="flex w-full justify-between items-center">
+          <ChevronLeft
+            className="w-[125px] h-[125px]"
+            style={{ color: Colors.Grey }}
+          />
+          <div className="flex justify-between items-center mt-10">
+            <img
+              src={require("./../../../../../Assets/Images/Phonological/Alphabets Module/Vowels/Aa.png")}
+              className="w-[374px] h-[263px] mr-10"
+            />
+            <img
+              src={require("./../../../../../Assets/Images/Phonological/Alphabets Module/Vowels/Apple.png")}
+              className="w-[286px] h-[313px]"
+            />
+          </div>
+          <button>
+            <ChevronRight
+              className="w-[125px] h-[125px]"
+              style={{ color: Colors.Black }}
+            />
+          </button>
+        </div>
+        {/* play */}
+        <div className="flex justify-center items-center">
+          <button>
+            <img
+              src={require("./../../../../../Assets/Images/Phonological/Alphabets Module/Vowels/Play.png")}
+              className="w-[138px] h-[138px] mt-10"
+            />
+          </button>
+        </div>
       </div>
+
+      {/* popup */}
+      {showPopup && (
+        <div
+          onClick={() => setShowPopup(false)}
+          className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+        >
+          {/* Popup Container */}
+          <div className="popup bg-white p-8 shadow-lg relative w-104 text-start">
+            <p
+              className="text-lg font-semibold font-[Nunito]"
+              style={{ color: Colors.Black }}
+            >
+              Welcome to the vowel lesson little one <br /> Learn the sounds of
+              the 5 vowel letters <br /> Play to hear the sounds <br /> Follow
+              along and do the quiz 😁
+            </p>
+
+            {/* Dog Image (positioned outside but touching bottom-left border of popup) */}
+            <img
+              src={require("./../../../../../Assets/Images/Phonological/Alphabets Module/Vowels/Owl.png")}
+              alt="Owl"
+              className="absolute -bottom-12 -left-20 w-20 h-20"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
